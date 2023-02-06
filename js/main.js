a2b543fb63614d7cb85f30cbbbe2e8045cf99679
Buchi-18 <3690@gmail.com>
2023-02-06 17:42:48 +0900

    add adobe-work img/ header WORKS fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@
 
 const headerLists = document.getElementById("headerLists");
 const mobileMenu = document.getElementById("mobileMenu");
+const works = document.getElementById("worksMenu");
+
 mobileMenu.addEventListener("click", function () {
+  toggleOpenClassName();
+});
+
+works.addEventListener("click", function () {
+  console.log("foo");
+  toggleOpenClassName();
+});
+
+function toggleOpenClassName() {
   headerLists.classList.toggle("open");
   mobileMenu.classList.toggle("open");
-});
+
+}

c77090fe326b7d675dcd5f65db530331e503a2cb
Buchi-18 <3690@gmail.com>
2023-03-25 15:27:18 +0900

    observer test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 "use strict";
-
-let innerHeight = window.innerHeight;
-const mainImage = document.getElementById("mainImage")
-
-mainImage.style.height = innerHeight + "px";
 //  ******************************
 //  menu bar
 //  ******************************
@@ -44,47 +39,95 @@ function menuBoardHandler() {
 //  ******************************
 //  animation handler js
 //  ******************************
-// フェードイン共通
-window.addEventListener("load", commonFade);
-window.addEventListener("scroll", commonFade);
-function commonFade() {
-  const fades = document.querySelectorAll(".fade");
-  fades.forEach(function (element) {
-    if (getInView(element)) {
-      element.classList.add("fade-in");
+// スライドイン
+const slideInItems = document.querySelectorAll(".slide-in-item");
+const slideItemObserver = new IntersectionObserver(setSlide);
+
+slideInItems.forEach(function (slideItem) {
+  slideItemObserver.observe(slideItem);
+});
+
+function setSlide(entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide-in");
     } else {
-      element.classList.remove("fade-in");
+      entry.target.classList.remove("slide-in");
     }
   });
 }
-// アニメーション width 発火
-window.addEventListener("scroll", setWidthAnim);
-function setWidthAnim() {
-  const widthBgAnim = document.querySelectorAll(".width-bg-anim");
-  widthBgAnim.forEach(function (element) {
-    if (getInView(element)) {
-      element.classList.add("width-animation");
+
+// window.addEventListener("load", commonFade);
+// window.addEventListener("scroll", commonFade);
+// function commonFade() {
+//   const fades = document.querySelectorAll(".slide-in-item");
+//   fades.forEach(function (element) {
+//     if (getInView(element)) {
+//       element.classList.add("slide-in");
+//     } else {
+//       element.classList.remove("slide-in");
+//     }
+//   });
+// }
+// アニメーション bg width 発火
+const widthBgAnimations = document.querySelectorAll(".width-bg-anim");
+const widthBgObserver = new IntersectionObserver(setWidthAnim);
+
+widthBgAnimations.forEach(function (widthBgAnim) {
+  widthBgObserver.observe(widthBgAnim);
+});
+
+function setWidthAnim(entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("width-animation");
     }
   });
 }
+
+// window.addEventListener("scroll", setWidthAnim);
+// function setWidthAnim() {
+//   const widthBgAnim = document.querySelectorAll(".width-bg-anim");
+//   widthBgAnim.forEach(function (element) {
+//     if (getInView(element)) {
+//       element.classList.add("width-animation");
+//     }
+//   });
+// }
+
 // アニメーション fade in scale 発火
-window.addEventListener("scroll", setScaleInAnim);
-function setScaleInAnim() {
-  const imgScaleAnim = document.querySelectorAll(".img-scale-anim");
-  imgScaleAnim.forEach(function (element) {
-    if (getInView(element)) {
-      element.classList.add("fade-in-scale");
+const imgScaleAnimations = document.querySelectorAll(".img-scale-anim");
+const fadeObserver = new IntersectionObserver(setFadeScale);
+
+imgScaleAnimations.forEach(function (imgScaleAnim) {
+  fadeObserver.observe(imgScaleAnim);
+});
+
+function setFadeScale(entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("fade-in-scale");
     }
   });
 }
+
+// window.addEventListener("scroll", setScaleInAnim);
+// function setScaleInAnim() {
+//   const imgScaleAnim = document.querySelectorAll(".img-scale-anim");
+//   imgScaleAnim.forEach(function (element) {
+//     if (getInView(element)) {
+//       element.classList.add("fade-in-scale");
+//     }
+//   });
+// }
 // 要素が画面内の任意の位置にあるか判定
-function getInView(element) {
-  const rect = element.getBoundingClientRect();
-  const rectHeight = rect.bottom - rect.top;
-  const isInView =
-    0 < rect.top && rect.bottom < window.innerHeight + rectHeight;
-  return isInView;
-}
+// function getInView(element) {
+//   const rect = element.getBoundingClientRect();
+//   const rectHeight = rect.bottom - rect.top;
+//   const isInView =
+//     0 < rect.top && rect.bottom < window.innerHeight + rectHeight;
+//   return isInView;
+// }
 
 //  ******************************
 //  loading image script

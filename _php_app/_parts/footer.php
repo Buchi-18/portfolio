8a54cd0b055b0452098eb21395439a91f433e338
Buchi-18 <3690@gmail.com>
2023-04-04 11:35:14 +0900

    Adding footer parts

---
+++ _php_app/_parts/footer.php
@@ -0,0 +1,102 @@
+<footer>
+  <div class="footer-head">
+    <div class="footer-logo"><a href="#"><img src="https://buchi.work/img/site-title.svg" alt="footer-logo"></a>
+    </div>
+    <div class="footer-social-links">
+      <ul>
+        <li>
+          <a href="https://github.com/Buchi-18" target="_blank" rel="noreferrer noopener nofollow">
+            <div class="icon-image">
+              <img style="width: 75%;" src="https://www.buchi.work/img/fontAwesome/github.svg" alt="github icon">
+            </div>
+            <span>github</span>
+          </a>
+        </li>
+
+        <li class="disabled" draggable="false">
+          <a href="#">
+            <div class="icon-image">
+              <img style="width: 60%;" src="https://www.buchi.work/img/fontAwesome/twitter.svg" alt="twitter icon">
+            </div>
+            <span>disabled</span>
+          </a>
+        </li>
+
+        <li class="disabled" draggable="false">
+          <a href="#">
+            <div class="icon-image">
+              <img style="width: 55%;" src="https://www.buchi.work/img/fontAwesome/tiktok.svg" alt="tiktok icon">
+            </div>
+            <span>disabled</span>
+          </a>
+        </li>
+
+        <li class="disabled" draggable="false">
+          <a href="#">
+            <div class="icon-image">
+              <img style="width: 60%;" src="https://www.buchi.work/img/fontAwesome/instagram.svg" alt="instagram icon">
+            </div>
+            <span>disabled</span>
+          </a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+
+  <div class="footer-wrap">
+    <div class="footer-site-map">
+      <div class="footer-bg-img"></div>
+      <!-- JavaScript Work -->
+      <ul>
+        <li class="page-title"><a href="https://buchi.work/pages/works/js-work.html">JavaScript&nbsp;Work</a></li>
+        <li><a href="https://buchi.work/pages/works/js-work.html#slider">Slick&nbsp;&rarr;&nbsp;Vanilla.js</a></li>
+        <li><a href="https://buchi.work/pages/works/js-work.html#modal">Lity&nbsp;&rarr;&nbsp;Vanilla.js</a></li>
+        <li><a
+            href="https://buchi.work/pages/works/js-work.html#cookie">Using&nbsp;cookie&nbsp;to&nbsp;...&nbsp;Vanilla.js</a>
+        </li>
+      </ul>
+      <!-- Adobe Work -->
+      <ul>
+        <li class="page-title"><a href="https://buchi.work/pages/works/adobe-work.html">Adobe&nbsp;Work</a></li>
+        <li><a href="https://buchi.work/pages/works/adobe-work.html#digitalMakeover">Digital&nbsp;makeover</a></li>
+        <li><a href="https://buchi.work/pages/works/adobe-work.html#combinePhoto">プリント加工</a></li>
+        <!-- <li><a href="">★★★★★★★★★</a></li> -->
+      </ul>
+      <!-- App Work -->
+      <ul>
+        <li class="page-title"><a href="https://buchi.work/pages/works/app-work.html">App&nbsp;Work</a></li>
+        <li><a href="https://buchi.work/pages/works/app-work.html#fakeWeather">Fake&nbsp;weather</a></li>
+        <li><a href="https://buchi.work/pages/works/app-work.html#keepImitationApp">Keep&nbsp;...Imitation&nbsp;app</a>
+        </li>
+        <li><a href="https://buchi.work/pages/works/app-work.html#passGenerator">Pass&nbsp;generator</a></li>
+      </ul>
+    </div>
+
+    <div class="sibling-links">
+      <ul class="sibling-item">
+        <li><a href="#">
+            <img src="https://dummyimage.com/150x150/000/fff&text=sibling" alt="sibling site">sibling site</a>
+        </li>
+
+        <li><a href="#">
+            <img src="https://dummyimage.com/150x150/000/fff&text=sibling" alt="sibling site">sibling site</a>
+        </li>
+
+        <li><a href="#">
+            <img src="https://dummyimage.com/150x150/000/fff&text=sibling" alt="sibling site">sibling site</a>
+        </li>
+
+        <li><a href="#">
+            <img src="https://dummyimage.com/150x150/000/fff&text=sibling" alt="sibling site">sibling site</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="footer-low">
+    <p class="policy"><a href="https://www.buchi.work/pages/privacy-policy.html">PRIVACY POLICY</a></p>
+    <span class="copy">copyright &copy; buchi.work All Rights Reserved.</span>
+  </div>
+</footer>
+

37b8ff2e4670908b368b2909977015f4d39cc7ec
Buchi-18 <3690@gmail.com>
2023-04-05 15:39:42 +0900

    included header/footer with php

--- pages/works/js-work.php
+++ pages/works/js-work.php
@@ -83,71 +83,9 @@
   <!-- ************************* -->
   <!-- header -->
   <!-- ************************* -->
-  <header>
-    <div class="header-wrap">
-      <h1 class="site-title">
-        <a href="https://www.buchi.work/"><img src="https://buchi.work/img/site-title.svg" alt="site-title"></a>
-      </h1>
-      <nav>
-        <ul id="pcMenuList">
-          <li><a href="https://www.buchi.work/index.html#works">WORKS</a></li>
-          <li><a href="https://www.buchi.work/index.html#about">ABOUT</a></li>
-          <li><a href="https://www.buchi.work/pages/readme.html">README</a></li>
-        </ul>
-      </nav>
-    </div>
-    <div id="menuBarBox">
-      <span class="menu-bar"></span>
-      <span class="menu-bar"></span>
-      <span class="menu-bar"></span>
-      <p id="menuBarText">more</p>
-    </div>
-    <!-- ***** menu board ***** -->
-    <div id="menuBoard">
-      <div class="menuBoard-wrap">
-        <div class="board-bg"></div>
-        <nav>
-          <ul>
-            <!-- WORKS -->
-            <li class="primary-menu"><a href="https://www.buchi.work/index.html#works">#WORKS</a>
-              <ul class="secondary-menu">
-                <li><a href="https://www.buchi.work/pages/works/js-work.html">javascript Works</a></li>
-                <li><a href="https://www.buchi.work/pages/works/adobe-work.html">Adobe Works</a></li>
-                <li><a href="https://www.buchi.work/pages/works/app-work.html">App Works</a></li>
-              </ul>
-            </li>
-
-            <!-- ABOUT -->
-            <li class="primary-menu"><a href="https://www.buchi.work/index.html#about">#ABOUT</a>
-              <ul class="secondary-menu">
-                <li><a href="https://www.buchi.work/index.html#personal">略歴</a></li>
-                <li><a href="https://www.buchi.work/index.html#experience">業務経験</a></li>
-                <li><a href="https://www.buchi.work/index.html#skill">開発スキル</a></li>
-              </ul>
-            </li>
-            <!-- README -->
-            <li class="primary-menu"><a href="https://www.buchi.work/pages/readme.html">#README</a>
-              <ul class="secondary-menu">
-                <li><a href="https://www.buchi.work/pages/readme.html#pageSpeedInsights">PageSpeed Insights 分析結果</a>
-                </li>
-                <li><a href="https://www.buchi.work/pages/readme.html#scoreGA4">スコアの改善 ( GA4 )</a></li>
-                <li><a href="https://www.buchi.work/pages/readme.html#scoreFonts">スコアの改善 ( webフォント )</a></li>
-                <li><a href="https://www.buchi.work/pages/readme.html#lazyLoadingImage">Lazy Loading の実装</a></li>
-                <li><a href="https://www.buchi.work/pages/readme.html#resultPuppeteer">Lazy Loading 実装テスト結果</a></li>
-                <li><a href="https://www.buchi.work/pages/readme.html#loadingAnimation">ローディング アニメーションの設置</a></li>
-                <li><a href="https://www.buchi.work/pages/readme.html#loadingScript">ローディング イメージ スクリプト</a></li>
-                <li><a href="https://www.buchi.work/pages/readme.html#loadingDemo">ローディング アニメーション DEMO</a></li>
-              </ul>
-            </li>
-            <li id="menuBarClose">
-              <span class="ja">閉じる</span>
-              <span class="en">close</span>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </header>
+  <?php
+  include("../../_php_app/_parts/_header.php");
+  ?>
 
   <!-- ************************* -->
   <!-- main image -->
@@ -423,111 +361,9 @@
   <!-- ************************* -->
   <!-- footer -->
   <!-- ************************* -->
-  <footer>
-    <div class="footer-head">
-      <div class="footer-logo"><a href="https://www.buchi.work/"><img src="https://buchi.work/img/site-title.svg"
-            alt="footer-logo"></a>
-      </div>
-      <div class="footer-social-links">
-        <ul>
-          <li>
-            <a href="https://github.com/Buchi-18" target="_blank" rel="noreferrer noopener nofollow">
-              <div class="icon-image">
-                <img style="width: 75%;" src="https://www.buchi.work/img/fontAwesome/github.svg" alt="github icon">
-              </div>
-              <span>github</span>
-            </a>
-          </li>
-
-          <li class="disabled" draggable="false">
-            <a href="#">
-              <div class="icon-image">
-                <img style="width: 60%;" src="https://www.buchi.work/img/fontAwesome/twitter.svg" alt="twitter icon">
-              </div>
-              <span>disabled</span>
-            </a>
-          </li>
-
-          <li class="disabled" draggable="false">
-            <a href="#">
-              <div class="icon-image">
-                <img style="width: 55%;" src="https://www.buchi.work/img/fontAwesome/tiktok.svg" alt="tiktok icon">
-              </div>
-              <span>disabled</span>
-            </a>
-          </li>
-
-          <li class="disabled" draggable="false">
-            <a href="#">
-              <div class="icon-image">
-                <img style="width: 60%;" src="https://www.buchi.work/img/fontAwesome/instagram.svg"
-                  alt="instagram icon">
-              </div>
-              <span>disabled</span>
-            </a>
-          </li>
-
-
-        </ul>
-      </div>
-    </div>
-
-    <div class="footer-wrap">
-      <div class="footer-site-map">
-        <div class="footer-bg-img"></div>
-        <!-- JavaScript Work -->
-        <ul>
-          <li class="page-title"><a href="https://buchi.work/pages/works/js-work.html">JavaScript&nbsp;Work</a></li>
-          <li><a href="https://buchi.work/pages/works/js-work.html#slider">Slick&nbsp;&rarr;&nbsp;Vanilla.js</a></li>
-          <li><a href="https://buchi.work/pages/works/js-work.html#modal">Lity&nbsp;&rarr;&nbsp;Vanilla.js</a></li>
-          <li><a
-              href="https://buchi.work/pages/works/js-work.html#cookie">Using&nbsp;cookie&nbsp;to&nbsp;...&nbsp;Vanilla.js</a>
-          </li>
-        </ul>
-        <!-- Adobe Work -->
-        <ul>
-          <li class="page-title"><a href="https://buchi.work/pages/works/adobe-work.html">Adobe&nbsp;Work</a></li>
-          <li><a href="https://buchi.work/pages/works/adobe-work.html#digitalMakeover">Digital&nbsp;makeover</a></li>
-          <li><a href="https://buchi.work/pages/works/adobe-work.html#combinePhoto">プリント加工</a></li>
-          <!-- <li><a href="">★★★★★★★★★</a></li> -->
-        </ul>
-        <!-- App Work -->
-        <ul>
-          <li class="page-title"><a href="https://buchi.work/pages/works/app-work.html">App&nbsp;Work</a></li>
-          <li><a href="https://buchi.work/pages/works/app-work.html#fakeWeather">Fake&nbsp;weather</a></li>
-          <li><a
-              href="https://buchi.work/pages/works/app-work.html#keepImitationApp">Keep&nbsp;...Imitation&nbsp;app</a>
-          </li>
-          <li><a href="https://buchi.work/pages/works/app-work.html#passGenerator">Pass&nbsp;generator</a></li>
-        </ul>
-      </div>
-
-      <div class="sibling-links">
-        <ul class="sibling-item">
-          <li><a href="#">
-              <img src="https://dummyimage.com/150x150/000/fff&text=sibling" alt="sibling site">sibling site</a>
-          </li>
-
-          <li><a href="#">
-              <img src="https://dummyimage.com/150x150/000/fff&text=sibling" alt="sibling site">sibling site</a>
-          </li>
-
-          <li><a href="#">
-              <img src="https://dummyimage.com/150x150/000/fff&text=sibling" alt="sibling site">sibling site</a>
-          </li>
-
-          <li><a href="#">
-              <img src="https://dummyimage.com/150x150/000/fff&text=sibling" alt="sibling site">sibling site</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="footer-low">
-      <p class="policy"><a href="https://www.buchi.work/pages/privacy-policy.html">PRIVACY POLICY</a></p>
-      <span class="copy">copyright &copy; buchi.work All Rights Reserved.</span>
-    </div>
-  </footer>
+  <?php
+  include("../../_php_app/_parts/_footer.php");
+  ?>
 
   <script src="../../js/main.js"></script>
   <script src="../../iFrames/js/iFrame.js"></script>

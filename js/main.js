ebb7bd5f6f9135f19d3777325d5945e44f7eeef4
Buchi-18 <3690@gmail.com>
2023-04-08 18:05:14 +0900

    delete comment/console.log

--- js/main.js
+++ js/main.js
@@ -40,10 +40,7 @@ function menuBoardHandler() {
 //  animation handler js
 //  ******************************
 // スライドイン
-
-//監視対象
 const slideInItems = document.querySelectorAll(".slide-in-item");
-// console.log(slideInItems);
 const slideItemObserver = new IntersectionObserver(setSlide);
 slideInItems.forEach(function (slideItem) {
   slideItemObserver.observe(slideItem);
@@ -53,7 +50,6 @@ function setSlide(entries) {
   entries.forEach(function (entry) {
     if (entry.isIntersecting) {
       entry.target.classList.add("slide-in");
-      console.log(entry.target);
     } else {
       entry.target.classList.remove("slide-in");
     }

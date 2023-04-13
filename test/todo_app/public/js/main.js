c10740bb221eb986947ba617268b3f200c9e4e74
Buchi-18 <3690@gmail.com>
2023-04-14 08:27:45 +0900

    perm test

--- test/todo_app/public/js/main.js
+++ test/todo_app/public/js/main.js
@@ -4,7 +4,7 @@
   const checkboxes = document.querySelectorAll("input[type='checkbox']");
   checkboxes.forEach(function (checkbox) {
     checkbox.addEventListener("change", function () {
-      checkbox.parentNode.submit();
+      fetch();
     });
   });
 

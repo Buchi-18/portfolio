27a6ff4b67b9662145db0335751fe8e2f7392d8b
Buchi-18 <3690@gmail.com>
2023-04-14 16:17:36 +0900

    Async communication implementation

--- test/todo_app/public/index.php
+++ test/todo_app/public/index.php
@@ -26,50 +26,30 @@ $todos = $todo->getAll();
 </head>
 
 <body>
-  <main>
+  <main data-token="<?= Utils::h($_SESSION['token']); ?>">
     <header>
     <h1>Todos</h1>
       <!-- CHECKED ALL DELETE -->
-      <form action="?action=purge" method="post">
-        <span class="purge">Purge</span>
-        <input type="hidden" name="token"
-          value="<?= Utils::h($_SESSION['token']); ?>">
-      </form> 
+      <span 
+        class="purge">
+        Purge
+      </span>
     </header>
 
-    <form action="?action=add" method="post">
+    <form>
       <input type="text" name="title" placeholder="Type new todo.">
-      <input type="hidden" name="token"
-        value="<?= Utils::h($_SESSION['token']); ?>">
     </form>
 
     <ul>
       <?php foreach ($todos as $todo): ?>
-      <li>
-        <!-- チェックボックス -->
-        <form action="?action=toggle" method="post">
-          <input type="checkbox"
-            <?= $todo->is_done ? 'checked' : ''; ?>>
-          <input type="hidden" name="id" value="<?= Utils::h($todo->id); ?>">
-          <input type="hidden" name="token"
-            value="<?= Utils::h($_SESSION['token']); ?>">
-        </form>
-
-        <!-- TODO タイトル -->
-        <span
-          class="<?= $todo->is_done ? 'done' : ''; ?>">
-          <?= Utils::h($todo->title); ?>
-        </span>
-
-        <!-- DELETE ボタン -->
-        <form action="?action=delete" method="post" class="delete-form">
+        <li data-id="<?= Utils::h($todo->id); ?>">
+          <!-- チェックボックス -->
+          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+          <!-- TODO タイトル -->
+          <span><?= Utils::h($todo->title); ?></span>
+          <!--  DELETE ボタン -->
           <span class="delete">×</span>
-          <input type="hidden" name="id" value="<?= Utils::h($todo->id); ?>">
-          <input type="hidden" name="token"
-            value="<?= Utils::h($_SESSION['token']); ?>">
-        </form>
-
-      </li>
+        </li>
       <?php endforeach;?>
     </ul>
   </main>
--- test/todo_app/public/js/main.js
+++ test/todo_app/public/js/main.js
@@ -1,26 +1,104 @@
 "use strict";
+
 {
-  // チェックボックスの判定
+  const token = document.querySelector("main").dataset.token;
+  const ul = document.querySelector("ul");
   const checkboxes = document.querySelectorAll("input[type='checkbox']");
-  checkboxes.forEach(function (checkbox) {
-    checkbox.addEventListener("change", function () {
-      fetch();
-    });
+  const input = document.querySelector("[name='title']");
+  input.focus();
+  const deletes = document.querySelectorAll(".delete");
+  const purge = document.querySelector(".purge");
+
+  function addTodo(id, title) {
+    let addHtml = `      
+        <li data-id="${id}">
+            <input type="checkbox">
+            <span>${title}</span>
+            <span class="delete">×</span>
+        </li>`;
+
+    ul.insertAdjacentHTML("afterbegin", addHtml);
+  }
+
+  ul.addEventListener("click", function (e) {
+    if (e.target.type === "checkbox") {
+      fetch("?action=toggle", {
+        method: "POST",
+        body: new URLSearchParams({
+          id: e.target.parentNode.dataset.id,
+          token: token,
+        }),
+      })
+        .then(function (response) {
+          if (!response.ok) {
+            throw new Error("This Todo has been deleted!");
+          }
+          return response.json();
+        })
+        .then(function (result) {
+          if (result.is_done !== e.target.checked) {
+            alert("This Todo has been updated! UI is begin updated.")
+            e.target.checked = result.is_done;
+            location.reload();
+          }
+  
+        })
+        .catch(function (err) {
+          alert(err.message);
+          location.reload();
+        });
+    }
+    if (e.target.classList.contains("delete")) {
+      if (!confirm("Are you sure?")) return;
+      fetch("?action=delete", {
+        method: "POST",
+        body: new URLSearchParams({
+          id: e.target.parentNode.dataset.id,
+          token: token,
+        }),
+      });
+
+      e.target.parentNode.remove();
+    }
   });
 
-  // DELETEボタンクリック時の処理
-  const deletes = document.querySelectorAll(".delete");
-  deletes.forEach(function (deleteBtn) {
-    deleteBtn.addEventListener("click", function () {
-      if (!confirm("Are you sure?")) return;
-      deleteBtn.parentNode.submit();
-    });
+  // タスクの追加
+  document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const title = input.value;
+
+    fetch("?action=add", {
+      method: "POST",
+      body: new URLSearchParams({
+        title: title,
+        token: token,
+      }),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        addTodo(json.id, title);
+      });
+
+    input.value = "";
+    input.focus();
   });
 
   // PURGEボタンクリック時の処理
-  const purge = document.querySelector(".purge");
   purge.addEventListener("click", function () {
     if (!confirm("Delete all checked tasks, Are you sure?")) return;
-    purge.parentNode.submit();
+    fetch("?action=purge", {
+      method: "POST",
+      body: new URLSearchParams({
+        token: token,
+      }),
+    });
+
+    checkboxes.forEach(function (checkbox) {
+      if (checkbox.checked) {
+        checkbox.parentNode.remove();
+      }
+    });
   });
 }
